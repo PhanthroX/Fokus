12c3101719066ea48237d2e55595dba30f62eba0
Matheus Mariano Rocha <105793853+PhanthroX@users.noreply.github.com>
2023-11-09 12:28:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
-const iniciarOuPausarBt = document.querySelector('#start-pause span')
-const cardPrimario = document.querySelector('.app__card-primary-butto-icon')
-const tempoNaTela = document.querySelector('#timer')
 const musicaFocoInput = document.querySelector('#alternar-musica')
-const musica = new Audio('\sons/luna-rise-part-one.mp3')
-musica.loop = true
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
+const tempoNaTela = document.querySelector('#timer')
 
-const somIniciar = new Audio('\sons/play.wav')
-const somPausar = new Audio('\sons/pause.mp3')
-const somParar = new Audio('\sons/beep.mp3')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
+musica.loop = true
+
 musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
         musica.play()
-    }   else {
+    } else {
         musica.pause()
     }
 })
@@ -52,33 +53,46 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `\imagens/${contexto}.png`)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
         case "foco":
-            titulo.innerHTML = `Otimize sua produtividade,<br>
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-                `
-
+            `
             break;
         case "descanso-curto":
-            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta.</strong>
-            `
-
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
             break;
-            case "descanso-longo":
-                titulo.innerHTML = `Hora de voltar à superficie. <strong class="app__title-strong">Faça uma pausa longa.</strong>
-                `
-                break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
         default:
             break;
     }
 }
 
 const contagemRegressiva = () => {
-    if(tempoDecorridoEmSegundos <= 0) {
-        // somParar.play()  // Esse som não é nada contemplativo pra usar em sistema pomodoro
-        alert('Tempo finalizado')
+    if (tempoDecorridoEmSegundos <= 0) {
         zerar()
+        const focoAtivo = html.getAttribute('data-contexto') === 'foco'
+        if (focoAtivo) {            
+            var event = new CustomEvent("TarefaFinalizada", {
+                detail: {
+                    message: "A tarefa foi concluída com sucesso!",
+                    time: new Date(),
+                },
+                bubbles: true,
+                cancelable: true
+            });
+            document.dispatchEvent(event);
+            tempoDecorridoEmSegundos = 5
+            mostrarTempo()
+        }
+
         return
     }
     tempoDecorridoEmSegundos -= 1
@@ -88,21 +102,21 @@ const contagemRegressiva = () => {
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
-    if (intervaloId) {
-        somPausar.play()
-        cardPrimario.setAttribute('src', '\imagens/play_arrow.png')
+    if(intervaloId){
+        audioPausa.play()
         zerar()
         return
     }
-    somIniciar.play()
-    cardPrimario.setAttribute('src', '\imagens/pause.png')
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
     iniciarOuPausarBt.textContent = "Pausar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
 }
 
 function zerar() {
-    clearInterval(intervaloId)
+    clearInterval(intervaloId) 
     iniciarOuPausarBt.textContent = "Começar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
     intervaloId = null
 }
 
@@ -113,4 +127,3 @@ function mostrarTempo() {
 }
 
 mostrarTempo()
-
